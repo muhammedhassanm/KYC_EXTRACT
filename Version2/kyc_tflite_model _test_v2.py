9add01a8e19c543ec027d7eb46f4f0c40cfa624d
muhammedhassanm <31989237+muhammedhassanm@users.noreply.github.com>
2020-01-24 16:28:31 +0530

    ytytyty

--- Version2/kyc_tflite_model _test_v2.py
+++ Version2/kyc_tflite_model _test_v2.py
@@ -8,15 +8,10 @@ import numpy as np
 import tensorflow as tf
 from keras.preprocessing.image import  array_to_img, img_to_array, load_img
 import pandas as pd
-import PIL.ImageColor as ImageColor
-import PIL.ImageDraw as ImageDraw 
-import PIL.ImageFont as ImageFont
 from matplotlib import pyplot as plt
 import cv2
 from imutils.video import VideoStream
 from imutils.video import FPS
-import time
-import imutils
 import pytesseract 
 
 size = (300, 300)
@@ -33,7 +28,7 @@ def show_image(IMAGE):
     plt.show()
 
 # Load TFLite model and allocate tensors.
-interpreter = tf.lite.Interpreter(model_path='E:/Desktop/KYC_EXTRACT/Version2/model_v2/aadhar_field_extraction_27_9_2019__model_tflite.tflite')
+interpreter = tf.lite.Interpreter(model_path='E:/KYC_EXTRACT/Version2/model_v2/kyc_detection_model_v2__30_10_2019_tflite.tflite')
 # interpreter = tf.lite.Interpreter(model_path='adhar_pan_license_17-10-19.tflite')
 interpreter.allocate_tensors()
 
@@ -44,16 +39,16 @@ output_details = interpreter.get_output_details()
 # Test model on random input data.
 input_shape = input_details[0]['shape']
 
-CLASSES = ['name','dob','gender','no','front','address','back']
+#CLASSES = ['name','dob','gender','no','front','address','back']
 #CLASSES =['permit_no','kitas_front']
-#CLASSES = ["cow", "cow_face", "unblurred_muzzle", "cow_eye", "cow_eye",
-#     "cow_ear", "cow_ear", "cow_eartag", "laptop", "mobile", "cup", "chair",
-#     "pen", "mouse", "monitor", "book", "bottle", "key_board",
-#     "tablet", "person", "eye_glass","cpu","blurred_muzzle", "adhar_front", "adhar_back", "pan", "license","atm","business_card","office_id"]
+CLASSES = ["cow", "cow_face", "unblurred_muzzle", "cow_eye", "cow_eye",
+     "cow_ear", "cow_ear", "cow_eartag", "laptop", "mobile", "cup", "chair",
+     "pen", "mouse", "monitor", "book", "bottle", "key_board",
+     "tablet", "person", "eye_glass","cpu","blurred_muzzle", "adhar_front", "adhar_back", "pan", "license","atm","business_card","office_id"]
 #CLASSES =['pan','pan_no','pan_name','pan_dob']
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
-image ='E:/Desktop/KYC_EXTRACT/Version2/test_images/Doc_1572511139779.jpeg'
+image ='C:/Users/100119/Desktop/Doc_1572262036073_naaa.jpeg'
 frame = cv2.imread(image)
 (h, w) = frame.shape[:2]
 cv2.imwrite('aa.jpg',frame)
@@ -72,10 +67,10 @@ Result_Data['class'] = list(output_data[0])
 output_data = interpreter.get_tensor(output_details[2]['index'])
 Result_Data['score'] = list(output_data[0])
 Result_Data['score'] = Result_Data['score'] * 100
-Result_Data['x1'] = (Result_Data['x1']*w).astype(int)
-Result_Data['y1'] = (Result_Data['y1']*h).astype(int)
-Result_Data['x2'] = (Result_Data['x2']*w).astype(int)
-Result_Data['y2'] = (Result_Data['y2']*h).astype(int)
+Result_Data['x1'] = (Result_Data['x1']*w)
+Result_Data['y1'] = (Result_Data['y1']*h)
+Result_Data['x2'] = (Result_Data['x2']*w)
+Result_Data['y2'] = (Result_Data['y2']*h)
 
 count =1
 for index,detection  in Result_Data.iterrows():
@@ -84,32 +79,23 @@ for index,detection  in Result_Data.iterrows():
             idx = int(detection['class'])
             (startX, startY, endX, endY) = detection['x1'].astype("int"),detection['y1'].astype("int"),detection['x2'].astype("int"),detection['y2'].astype("int")
             crop=frame[startY:endY,startX:endX]
-            crop = cv2.resize(crop,None,fx=2.5,fy =2.5)
-            cv2.imwrite('E:/Desktop/KYC_EXTRACT/Version1/test-images/' +str(count)+'.jpg',crop)
-            count+=1
-#                gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
-#                retval, thresh_gray = cv2.threshold(g, thresh=75, maxval=255,type=cv2.THRESH_BINARY_INV)
-            cv2.imshow('crop',crop)
-            cv2.waitKey(0)
-#            if int(detection['class']) != 4 and int(detection['class']) != 6 :
-#                print(int(detection['class']))
-#                custom_oem_psm_config = '--psm 6'
-#                text= pytesseract.image_to_string(crop,config = custom_oem_psm_config)
+#            crop = cv2.resize(crop,(600,600),fx=2.5,fy =2.5)
+            cv2.imwrite('E:/Desktop/' +str(count)+'.jpg',crop)
+            
+
             if int(detection['class']) != 0:
                
                 print(int(detection['class']))
-               
                 text= pytesseract.image_to_string(crop,config = '--psm 6')
-                
-                print(text)
-            #cv2.imshow('crop',crop)
-            #cv2.waitKey(0)
+                print(text)  
+            cv2.imshow('crop',crop)
+            cv2.waitKey(0)
             # draw the prediction on the frame
             label = "{}: {:.2f}%".format(CLASSES[idx],confidence)
             cv2.rectangle(frame, (startX, startY), (endX, endY),COLORS[idx], 3)
             y = startY - 15 if startY - 15 > 15 else startY + 15
             cv2.putText(frame, label, (startX, y),cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 4)
-
+            count+=1 
 cv2.imshow("Frame", cv2.resize(frame,(400,400)))
 cv2.waitKey(0)
 
